97e6d54dd36180bf99b8e5d1cf7077560c155116
Brian96086 <brian96086@gmail.com>
2021-10-11 14:07:57 -0700

    pushed PA3 startercode

---
+++ PA3/Autograder.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+
+public class Autograder implements MessageExchange {
+
+    // time allowed
+    private static final int DEFAULT_ALLOTTED_TIME = 15;
+
+    // max number of messages to fetch
+    private static final int MAX_MSG_SIZE = 100;
+
+    // Error message to use in OperationDeniedException
+    protected static final String SESSION_ENDED =
+            "Session has already ended. Ticket can't be resolved";
+
+
+
+    // instance variables
+    private ArrayList<User> users;
+    private ArrayList<Message> log;
+    private ArrayList<String> results;
+    private Tutor tutor;
+
+    public Autograder(Tutor tutor) {
+        /* TODO */
+    }
+
+    public ArrayList<Message> getLog(User requester) {
+        /* TODO */
+        return null;
+    }
+    public ArrayList<String> getResults(){
+        /* TODO */
+        return null;
+
+    }
+
+    public boolean addUser(User u) {
+        /* TODO */
+        return false;
+    }
+
+    public boolean removeUser(User requester, User u) {
+        /* TODO */
+        return false;
+    }
+
+    public ArrayList<User> getUsers() {
+        /* TODO */
+        return null;
+    }
+
+    public boolean recordMessage(Message m) {
+        /* TODO */
+        return false;
+    }
+
+    public String resolveTicket() throws OperationDeniedException {
+        /* TODO */
+        return null;
+    }
+    public boolean stopSession(){
+        return false;
+    }
+
+}
